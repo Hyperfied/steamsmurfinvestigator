cebb4b95867ac3e89358e6077ff45f756eff28d4
Hyperfied <hyperfied@outlook.com>
2025-03-17 17:09:39 +0000

    Let server return website files

--- server/server.py
+++ server/server.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 import StatsProcessor
 
 app = FastAPI()
 
 @app.get("/")
-async def hello_world():
-    return {"response": "Hello World"}
+async def get_page():
+    return FileResponse("../client/index.html")
+
+@app.get("/style.css")
+async def get_style():
+    return FileResponse("../client/style.css")
+
+@app.get("/script.js")
+async def get_script():
+    return FileResponse("../client/script.js")
 
 @app.get("/profile/{steamid}")
 async def profile(steamid: str):
@@ -19,6 +28,7 @@ async def profile(steamid: str):
     }
     
     return response
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000)
